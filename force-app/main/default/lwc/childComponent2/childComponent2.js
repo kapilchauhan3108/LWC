c95597f40735945bbab57d0981e8036fd334b504
Kapil Singh. Chauhan <kapil.chauhan@AD.SAASFOCUS.COM>
2020-06-30 20:17:36 +0530

    Communication Between components

--- force-app/main/default/lwc/childComponent2/childComponent2.js
+++ force-app/main/default/lwc/childComponent2/childComponent2.js
@@ -2,9 +2,20 @@ import { LightningElement ,track , api} from 'lwc';
 
 export default class ChildComponent2 extends LightningElement {
 
+ //Creating a public component .. and define the default values ...
+   @api meetingRoomInfo = {roomNumber : 'A-01' , roomCapacity : 10} ;
+
+   @api someBoolean  = "false" ;
+
 
     @api publicMethod(){
         console.log('Checkbox is checked ');
         alert('checkbox is checked');
+    }
+
+
+    tileClicked(){       
+        const tileCliked = new CustomEvent('tileclick' , {detail : this.meetingRoomInfo , bubbles :true}); //  'tileClick' is the name of the event
+        this.dispatchEvent(tileCliked); // dispatchEvent is a JavaScript method to fire event ....
     }
 }
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -3,22 +3,33 @@ import { LightningElement , track } from 'lwc';
 export default class ParentComponent extends LightningElement {
 
     meetingRoomInfo = [
-        {roomNumber : 'A-04' , roomCapacity : 8},
+        {roomNumber : 'A-01' , roomCapacity : 25},
         {roomNumber : 'A-02' , roomCapacity : 15},
         {roomNumber : 'A-03' , roomCapacity : 20},
+        {roomNumber : 'A-04' , roomCapacity : 8},
     ];
-
-
-
     someBoolean = "true" ;
 
     hotelInfo = 'All kind of roomes are available';
 
+   @track meetingRoomSelected;
+
     checkboxClickParent(){
         console.log('clicked in paremt ');
-
         const childComponent =  this.template.querySelector('c-child-component2');
         console.log('child comp' , childComponent);
         childComponent.publicMethod(); 
+ 
     }
+
+    handleTileClick(event){
+        const meetingRoom = event.detail;
+        this.meetingRoomSelected  = meetingRoom.roomNumber;
+    }
+
+    constructor(){
+        super();
+        this.template.addEventListener('tileclick' , this.handleTileClick.bind(this));
+    }
+    
 }
